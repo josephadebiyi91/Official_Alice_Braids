7c3a9904b32c08e744d41284a9edfce7b238a786
josephadebiyi91 <97675443+josephadebiyi91@users.noreply.github.com>
2022-11-22 09:33:50 -0800

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,41 @@
+// navbar toggle
+$(document).ready(function(){
+    $(".navbar-toggler").click(function(){
+     $(".navbar-collapse").show()
+    })
+    $(".nav-item").click(function(){
+      $(".navbar-collapse").hide()
+    })
+  })
+// end
+
+ 
+var swiper = new Swiper(".slide-content", {
+  slidesPerView: 3,
+  spaceBetween: 25,
+  loop: true,
+  centerSlide: 'true',
+  fade: 'true',
+  grabCursor: 'true',
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints:{
+      0: {
+          slidesPerView: 1,
+      },
+      520: {
+          slidesPerView: 2,
+      },
+      950: {
+          slidesPerView: 3,
+      },
+  },
+});
